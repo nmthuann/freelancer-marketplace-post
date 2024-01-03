d6883bfa88dd48814b639448ee321ca1393a5c34
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2024-01-03 23:35:41 +0700

    commit_3_1_24
    - Update file.

--- src/modules/category/category.entity.ts
+++ src/modules/category/category.entity.ts
@@ -5,7 +5,7 @@ import { MajorEntity } from "../major/entities/major.entity"
 
 
 @Entity({name:'Categories'})
-export class CategoryEntity extends BaseEntity {  //extends BaseEntity
+export class CategoryEntity extends BaseEntity {
     @PrimaryGeneratedColumn()
     category_id: number
 
@@ -13,7 +13,21 @@ export class CategoryEntity extends BaseEntity {  //extends BaseEntity
     category_name: string
 
     @Column()
-    description: string
+    description: string;
+
+    /**
+     * Kỹ thuật: Nested Set Model 
+     * (1, 'Electronics', 1, 10),
+     * (2, 'Mobile Phones', 2, 5),
+     * (3, 'Laptops', 6, 9),
+     * (4, 'Smartphones', 3, 4),
+     * (5, 'Gaming Laptops', 7, 8);
+     */
+    @Column({ nullable: false })
+    left_value: number;
+
+    @Column({ nullable: false })
+    right_value: number;
 
     @OneToMany(
         () => MajorEntity,
